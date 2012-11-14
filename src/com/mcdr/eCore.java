53a1515d70c58c44287879e56219c9ffa7fc7cee
Wim de With <wfdewith@gmail.com>
2012-11-14 19:31:02 +0100

    Build 0

---
+++ src/com/mcdr/eCore.java
@@ -0,0 +1,21 @@
+package com.mcdr;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class eCore extends JavaPlugin {
+
+	public void onEnable(){
+		
+	}
+	
+	public void onDisable(){
+		
+	}
+	
+	public boolean onCommand(CommandSender sndr, Command cmd, String lbl, String[] args){
+		return true;		
+	}
+
+}
