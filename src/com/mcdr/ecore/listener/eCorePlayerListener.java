3a4cafe048182153e40f0ead80fc360f99e4cd48
Jorai Rijsdijk <jorairijsdijk@gmail.com>
2012-11-20 21:15:18 +0100

    Teleport v1.0
    Teleport with smoke effect now working.

--- src/com/mcdr/ecore/listener/eCorePlayerListener.java
+++ src/com/mcdr/ecore/listener/eCorePlayerListener.java
@@ -1,11 +1,17 @@
 package com.mcdr.ecore.listener;
 
+import java.util.Random;
+
+import org.bukkit.Effect;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
 
 import com.mcdr.ecore.eCore;
 import com.mcdr.ecore.task.TaskManager;
@@ -15,7 +21,7 @@ public class eCorePlayerListener implements Listener {
 	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
 	public void onPlayerJoin(PlayerJoinEvent e) {
 		Player player = e.getPlayer();
-		if (player.getName() == eCore.name){
+		if (player.getName().equalsIgnoreCase(eCore.name)){
 			TaskManager.startFlameEffect();
 		}
 	}
@@ -23,8 +29,21 @@ public class eCorePlayerListener implements Listener {
 	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
 	public void onPlayerQuit(PlayerQuitEvent e) {
 		Player player = e.getPlayer();
-		if (player.getName() == eCore.name){
+		if (player.getName().equalsIgnoreCase(eCore.name)){
 			TaskManager.stopFlameEffect();
+		}
+	}
+	
+	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = false)
+	public void onTeleport(PlayerTeleportEvent e){
+		Player player = e.getPlayer();
+		if (player.getName().equalsIgnoreCase(eCore.name)){
+			World world = player.getWorld();
+			Location loc = player.getLocation();
+			Random rand = new Random();
+			for(int i = 0;i<100;i++)
+				for(double j = 0.0; j <= 2.0; j+=0.25)
+					world.playEffect(loc.clone().add(0.0, j, 0.0),Effect.SMOKE, i, 16);
 		}
 	}
 }
