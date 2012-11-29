3ec997dd3e6b09bf5f1b362b1fbe127b11676667
Jorai Rijsdijk <jorairijsdijk@gmail.com>
2012-11-29 19:05:51 +0100

    Cleaned up the merge of the SignChange branch

--- src/com/mcdr/ecore/eCore.java
+++ src/com/mcdr/ecore/eCore.java
@@ -22,13 +22,7 @@ public class eCore extends JavaPlugin {
 	public static BukkitScheduler scheduler;
 	public static PluginManager pm;
 	public static Server server;
-<<<<<<< HEAD
-	public static String name = "Kraeghnor";
-	
-	private eCoreButtonListener csl;
-=======
 	public static String name;
->>>>>>> origin/SignChange
 
 	
 	public eCore(){
